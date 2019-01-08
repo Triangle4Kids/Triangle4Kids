d5c04acd0e69d09cbe4a934c0006604665462cc8
Ardra Salemy <ardrasalemy@gmail.com>
2019-01-08 13:19:53 -0500

    first commit to this branch

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from core.models import LeaveReview
+from django import forms
 
 
 class LeaveReviewForm(ModelForm):
@@ -7,4 +8,17 @@ class LeaveReviewForm(ModelForm):
        model = LeaveReview
        fields = ['reviewer', 'text', 'rating']
 
-       
+
+class EventForm(forms.Form):
+    title = forms.CharField(label='Event/Class/Camp Name', max_length=255)
+    cost = 
+    category = 
+    description = forms.TextField(label='description')
+    date = forms.DateField(, required=False)
+    time = forms.TimeField(, required=False)
+    age =
+    link = forms.URLField(, required=False)
+    phone =
+    email = forms.EmailField(, required=False)
+    address = 
+    photo = 
