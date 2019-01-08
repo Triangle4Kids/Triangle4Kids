5aad3caf92d171ed7bf950db161b38ce1ca12956
David Dean <david@davidlndean.com>
2019-01-08 14:35:32 -0500

    added Procfile, Gunicorn, Heroku and prep for deploy

--- triangle4kids/settings.py
+++ triangle4kids/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,6 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_PORT = 1025
 LOGIN_REDIRECT_URL = "home"
+
+# Activate django_heroku
+django_heroku.settings(locals())
