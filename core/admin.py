7a4025c8c86f45e636eb2a355b6e9e66ffb0fcd0
Sam LoCascio <sflocascio@gmail.com>
2019-01-07 23:59:12 -0500

    commit includes login, index page, event detail page, bus detail page, user account, reviews, and bootstrap integration. Submit user review still not working properly

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from core.models import Profile, Event, Business, LeaveReview
+# Register your models here.
+
+class EventsInLine(admin.StackedInline):
+    model = Event
+    list_display = ("title", "address", "link", "description", "date_of_event", "time_range", "start_date", "end_date", "city", "state", "start_time", "end_time")
+    prepopulated_fields = {'slug': ('title',)}
+
+class BusinessAdmin(admin.ModelAdmin):
+    model = Business
+    list_display = ("name", "address", "average_rating", "city", "state", "link")
+    inlines = [EventsInLine]
+    prepopulated_fields = {'slug': ('name',)}
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = LeaveReview
+    list_display = ("reviewer", "text", "rating", "business", 'event')
+
+admin.site.register(Business, BusinessAdmin)
+admin.site.register(LeaveReview, ReviewAdmin)
--- triangle4kids/settings.py
+++ triangle4kids/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     #Third Party Apps
     'django_extensions',
     'debug_toolbar',
+    'registration',
     
 ]
 
@@ -130,3 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ACCOUNT_ACTIVATION_DAYS = 7 
+STATIC_URL = '/static/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
+LOGIN_REDIRECT_URL = "home"
+
