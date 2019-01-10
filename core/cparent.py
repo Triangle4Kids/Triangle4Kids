301805ed0ecffd414108b84bef84c6c43b627f07
David Dean <david@davidlndean.com>
2019-01-10 16:21:40 -0500

    added cparent.py and started work on webscraping

---
+++ core/cparent.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+
+source = requests.get('http://www.carolinaparent.com/CP/Camp-Listings/index.php?').text
+    
+soup = BeautifulSoup(source, 'lxml')
+
+print(soup.prettify())
+
+# for article in soup.find_all('div', class_='business-name')
+
+
+# headline = article.h2.a.text
+
+
+# def simple_get(url):
+#     """
+#     Attempts to get the content at `url` by making an HTTP GET request.
+#     If the content-type of response is some kind of HTML/XML, return the
+#     text content, otherwise return None.
+#     """
+#     try:
+#         with closing(get(url, stream=True)) as resp:
+#             if is_good_response(resp):
+#                 return resp.content
+#             else:
+#                 return None
+
+#     except RequestException as e:
+#         log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+#         return None
+
+
+# def is_good_response(resp):
+#     """
+#     Returns True if the response seems to be HTML, False otherwise.
+#     """
+#     content_type = resp.headers['Content-Type'].lower()
+#     return (resp.status_code == 200 
+#             and content_type is not None 
+#             and content_type.find('html') > -1)
+
+
+# def log_error(e):
+#     """
+#     It is always a good idea to log errors. 
+#     This function just prints them, but you can
+#     make it do anything.
+#     """
+#     print(e)
