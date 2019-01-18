d147a9e933d5114e1817734b2b7e305ebb783770
Sam LoCascio <sflocascio@gmail.com>
2019-01-18 15:50:01 -0500

    start of aws

--- triangle4kids/settings.py
+++ triangle4kids/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'bootstrapform',
     'phone_field',
     'rest_framework',
+    'storages', 
 ]
 
 MIDDLEWARE = [
@@ -158,6 +159,12 @@ REST_FRAMEWORK = {
 
 
 
+
+
+
+
+
+
 ACCOUNT_ACTIVATION_DAYS = 7
 STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -172,3 +179,26 @@ INTERNAL_IPS = ['127.0.0.1']
 
 # Activate django_heroku
 django_heroku.settings(locals())
+
+
+if not DEBUG:
+    STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+    ]
+
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+
+    AWS_LOCATION = 'static'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+    DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+
+
