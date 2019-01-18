7e6515fe57620430d0fb34c922beb79b5398a50b
Ardra Salemy <ardrasalemy@gmail.com>
2019-01-18 10:56:05 -0500

    Searches work now, had to remove drop-down searches.

--- triangle4kids/settings.py
+++ triangle4kids/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'triangle4kids.urls'
--- triangle4kids/urls.py
+++ triangle4kids/urls.py
@@ -113,4 +113,3 @@ if settings.DEBUG:
     path('api/', include('rest_framework.urls')),
     path('api/', include((router.urls, 'core'), namespace="api")),
     url(r'^api/(?P<pk>[0-9]+)/$', api_views.BusinessDetailViewset)
-    
