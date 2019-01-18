6d9163d7e55e278d3e679d5de88f7e601133fb90
Sam LoCascio <sflocascio@gmail.com>
2019-01-18 16:23:50 -0500

    Fix bug in settings

--- triangle4kids/heroku_settings.py
+++ triangle4kids/heroku_settings.py
@@ -17,6 +17,6 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'triangle4kids.storage_backends.MediaStorage'
 
 
